8fc04f8bb94e0471121012179c17cbfe04da4b32
ValentinaRippe <lauravalentina200102@gmail.com>
2022-02-16 18:09:02 -0500

    Carousel-styles

--- marketplace_frontend/src/components/Carousel/CarouselContainer.jsx
+++ marketplace_frontend/src/components/Carousel/CarouselContainer.jsx
@@ -1,50 +1,48 @@
 import React from 'react'
 import { Carousel } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
+import style from './CarouselContainer.module.css'
 
 export function CarouselContainer() {
   return (
     <div>
-      <Carousel>
+      <Carousel variant="dark">
   <Carousel.Item>
     <Link to='/details'>
     <img
-      className="d-block w-50"
+      className={style.img_carousel}
       src="https://cdn.shopify.com/s/files/1/0511/6592/1467/products/vegan-shoes-beflamboyant-manimal-black-side-view_1024x1024@2x.jpg?v=1637142787"
       alt="First slide"
     />
     </Link>
     <Carousel.Caption>
-      <h3>First slide label</h3>
-      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+      <h3>Zapatillas veganas Manimal Blackl</h3>
     </Carousel.Caption>
   </Carousel.Item>
   <Carousel.Item>
   <Link to='/details'>
     <img
-      className="d-block w-50"
+      className={style.img_carousel}
       src="https://cdn.shopify.com/s/files/1/0511/6592/1467/products/vegan-shoes-beflamboyant-ux68-leopard-side-view_1024x1024@2x.jpg?v=1643442921"
       alt="Second slide"
     />
   </Link>
   
     <Carousel.Caption>
-      <h3>Second slide label</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <h3>Zapatos veganos de leopardo</h3>
     </Carousel.Caption>
   </Carousel.Item>
   <Carousel.Item>
   <Link to='/details'>
     <img
-      className="d-block w-50"
+      className={style.img_carousel}
       src="https://cdn.shopify.com/s/files/1/0511/6592/1467/products/vegan-sneakers-caramel-side_1024x1024@2x.jpg?v=1644169671"
       alt="Third slide"
 
     />
 </Link>
     <Carousel.Caption>
-      <h3>Third slide label</h3>
-      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+      <h3>Zapatos veganos de color caramelo</h3>
     </Carousel.Caption>
   </Carousel.Item>
 </Carousel>
