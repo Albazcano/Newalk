e38957b71de8c54639cf1bf67f6d8d077aa5c6e1
RosaPs <rosapacheco.contact@gmail.com>
2022-02-16 14:09:25 +0100

    Añadiendo files para el comienzo

---
+++ MarketPlace_backEnd/src/main/java/com/example/marketplace_backend/MarketPlaceBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.marketplace_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketPlaceBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketPlaceBackEndApplication.class, args);
+    }
+
+}
