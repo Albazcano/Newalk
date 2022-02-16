5f37e787041c825414bf6daa06ba3921930db4c4
ValentinaRippe <lauravalentina200102@gmail.com>
2022-02-16 15:28:52 -0500

    view paths were created, testing was installed

--- marketplace_frontend/src/Routes/Routes.jsx
+++ marketplace_frontend/src/Routes/Routes.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { Home } from '../../../../../react/catalogo/src/Views/Home';
+import { Footer } from '../components/Footer/Footer';
+import { Header } from '../components/Header/Header'
+import { Details } from '../views/Details/Details';
 
 export const Routes = () => {
   return (
-    <div>Routes</div>
+   <HashRouter>
+     <Header/>
+     <Routes>
+       <Route path='/' element={<Home/>}/>
+       <Route path='/filter' element={<Filter/>}/>
+       <Route path='/details' element={<Details/>}/>
+       <Route path='*' element={<Navigate replace to='/' />} />
+     </Routes>
+     <Footer/>
+   </HashRouter>
   );
 }
