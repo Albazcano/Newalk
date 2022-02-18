5622f1e70da57c78d13bfce5f5d7fe39e3419924
Jeiner Alexis Bustos Quipo <78630542+bustosjeiner@users.noreply.github.com>
2022-02-17 22:40:36 -0500

    styles header and footer

--- marketplace_frontend/src/components/Footer/Footer.jsx
+++ marketplace_frontend/src/components/Footer/Footer.jsx
@@ -3,9 +3,9 @@ import style from './Footer.module.css';
 
 export const Footer = () => {
   return (
-    <footer>
-      <div>
-        <p>
+    <footer className={style.footer}>
+      <div className={style.interestedContainer}>
+        <p className={style.links}>
           PRIVACIDAD <br />
           COOKIES <br />
           TÉRMINOS Y CONDICIONES <br />
@@ -15,12 +15,24 @@ export const Footer = () => {
           TRACKING
         </p>
       </div>
-      <div>
-        <p>
+      <div className={style.socialContainer}>
+        <p className={`${style.links} ${style.socialText}`}>
           INSTAGRAM <br />
           FACEBOOR <br />
           TWITTER <br />
           YOUTUBE <br />
+        </p>
+      </div>
+      <div className={style.copy}>
+        <div className={style.containerLogo}>
+          <img src='' alt='Logo Newalk' className={style.logo}></img>
+        </div>
+        
+        <p className={style.copyText}>
+          © 2022, NeWalk Todos los derechos reservados
+        </p>
+        <p className={style.copyText}>
+          Desarrollado por Equipo 10 hackathon Adevinta 
         </p>
       </div>
     </footer>
--- marketplace_frontend/src/components/NavBar/NavBar.jsx
+++ marketplace_frontend/src/components/NavBar/NavBar.jsx
@@ -2,32 +2,33 @@ import React from "react";
 import { Navbar, Nav, NavDropdown } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import style from './NavBar.module.css';
 
 export const NavBar = () => {
   return (
     <Navbar collapseOnSelect expand="lg" bg="ligth">
-      <Navbar.Brand href="#home">
-        Dice Roller
-      </Navbar.Brand>
+      <Link to="/home" className={style.containerImage}>
+        <img src="" alt='Logo Newalk'></img>
+      </Link>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="mr-auto">
-          <Link to="/home">Home</Link>         
+          <Link to="/home" className={style.links}>Inicio</Link>         
           <NavDropdown.Divider />
-          <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+          <NavDropdown title="Marcas" id="collasible-nav-dropdown">
             <NavDropdown.Item>
-              <Link to = '/filter'>
-                Action
+              <Link to = '/filter' className={style.links}>
+                Marca 1
               </Link>
             </NavDropdown.Item>
             <NavDropdown.Item>
-              <Link to = '/filter'>
-                Action
+              <Link to = '/filter' className={style.links}>
+                Marca 2
               </Link>
             </NavDropdown.Item>
             <NavDropdown.Item>
-              <Link to = '/filter'>
-                Action
+              <Link to = '/filter' className={style.links}>
+                Marca 3
               </Link>
             </NavDropdown.Item>
           </NavDropdown>
